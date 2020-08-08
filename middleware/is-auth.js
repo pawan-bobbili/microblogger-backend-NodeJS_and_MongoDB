db95e48b1a90b98be931ee288a8d87ab41e5f688
pawan-bobbili <pawanbob007@gmail.com>
2020-08-09 05:21:20 +0530

    Security bug fixed

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,3 +1,4 @@
+const { bcryptSecret } = require("../apikeys");
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
@@ -10,7 +11,7 @@ module.exports = (req, res, next) => {
   }
   token = token.split(" ")[1];
   try {
-    decodedToken = jwt.verify(token, "Karnal@18");
+    decodedToken = jwt.verify(token, bcryptSecret);
   } catch (err) {
     err.statusCode = 500;
     throw err;
